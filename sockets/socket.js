180ee458d71d8ba4989cc63806257ea93dfd52dc
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2021-03-01 18:45:59 -0500

    Implemento CRUD de bandas.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,15 +1,38 @@
 const { io } = require('../index');
 
+const BandModel = require('../models/band.model');
+const BandsCollection = require('../models/bands.collection');
+
+const bandsCollection = new BandsCollection();
+
+bandsCollection.addBand(new BandModel('Heroes del silencio'));
+bandsCollection.addBand(new BandModel('Megadeth'));
+bandsCollection.addBand(new BandModel('AC/DC'));
+bandsCollection.addBand(new BandModel('Scorpions'));
+
 // Socket
 io.on('connection', client => {
     console.log("Cliente conectado");
-    // client.on('event', data => { /* … */ });
+
+    client.emit('bandas', bandsCollection.getBands());
+
     client.on('disconnect', () => {
         console.log("Cliente desconectado");
     });
 
-    client.on('message', (payload) => {
-        console.log(`Recibiendo mensaje`, payload);
-        io.emit('message', { status: 200 })
+    client.on('add-band', (payload) => {
+        bandsCollection.addBand(new BandModel(payload['name']));
+        io.emit('bandas', bandsCollection.getBands());
     });
+
+    client.on('delete-band', (payload) => {
+        bandsCollection.deleteBand(payload['id']);
+        io.emit('bandas', bandsCollection.getBands());
+    });
+
+    client.on('vote', (payload) => {
+        bandsCollection.voteBand(payload['id']);
+        io.emit('bandas', bandsCollection.getBands());
+    });
+
 });
